d2a46355ebe6a3bec81a285c30c2c029300c583c
SquirtleSquad1988 <alexanderjayrobles0@gmail.com>
2017-03-28 09:37:51 -0400

    Update blog and comment serializer
    Updated comment and blog serializer so the response from the server
    gives me more information to add for handlebars and whatnot.

--- app/serializers/blog_serializer.rb
+++ app/serializers/blog_serializer.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class BlogSerializer < ActiveModel::Serializer
-  attributes :id, :title, :body, :editable
+  attributes :id, :title, :body, :updated_at, :user_id, :editable
   def editable
     scope == object.user
   end
--- app/serializers/comment_serializer.rb
+++ app/serializers/comment_serializer.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class CommentSerializer < ActiveModel::Serializer
-  attributes :id, :comment, :editable
+  attributes :id, :comment, :updated_at, :user_id, :blog_id, :editable
   def editable
     scope == object.user
   end
