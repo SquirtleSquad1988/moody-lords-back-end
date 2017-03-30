85abcaad015e6dbfc7f3f04e5e5d6f9818f36817
SquirtleSquad1988 <alexanderjayrobles0@gmail.com>
2017-03-30 10:53:36 -0400

    Add admin attribute to users
    Added admin attribute to users which is a boolean. In the migration,
    the default value is set to false. In examples.rb I am creating an user
    with an admin value of true. There is no way to create a user with an
    admin value of true on the front end.

--- db/examples.rb
+++ db/examples.rb
@@ -17,3 +17,36 @@
 #                password: 'abc123',
 #                password_confirmation: nil)
 # end
+
+User.create!(email: 'q@q',
+             password: 'q',
+             password_confirmation: 'q',
+             admin: true)
+#
+# %w(Revolver
+#    Please_Please_Me
+#    Let_It_Be
+#    Abbey_Road
+#    Meet_The_Beatles
+#    Help!
+#    A_Hard_Days_Night
+#    Rubber_Soul
+#    Magical_Mystery_Tour
+#    White_Album
+#    Sgt_Peppers_Lonely_Hearts_Club_Band
+#    Love
+#    Beatles_For_Sale
+#    Yellow_Submarine).each do |name|
+#   album = name.to_s
+#   Album.create!(artist_name: 'The Beatles',
+#                 album_name: album,
+#                 user: User.find(1))
+# end
+#
+# %w(antony jeff matt jason alex chelsea dave harry).each do |name|
+#   email = "#{name} is my friend"
+#   # next if User.exists? email: email
+#   Comment.create!(comment: email,
+#                   user: User.find(1),
+#                   album_id: '1')
+# end
