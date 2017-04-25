3e3f31641809cb14cf7cf81102c5a35f5df12321
SquirtleSquad1988 <alexanderjayrobles0@gmail.com>
2017-04-25 16:13:13 -0400

    Add record seed data

--- db/examples.rb
+++ db/examples.rb
@@ -23,25 +23,26 @@ User.create!(email: 'q@q',
              password_confirmation: 'q',
              admin: true)
 #
-# %w(Revolver
-#    Please_Please_Me
-#    Let_It_Be
-#    Abbey_Road
-#    Meet_The_Beatles
-#    Help!
-#    A_Hard_Days_Night
-#    Rubber_Soul
-#    Magical_Mystery_Tour
-#    White_Album
-#    Sgt_Peppers_Lonely_Hearts_Club_Band
-#    Love
-#    Beatles_For_Sale
-#    Yellow_Submarine).each do |name|
-#   album = name.to_s
-#   Album.create!(artist_name: 'The Beatles',
-#                 album_name: album,
-#                 user: User.find(1))
-# end
+%w(Revolver
+   Please_Please_Me
+   Let_It_Be
+   Abbey_Road
+   Meet_The_Beatles
+   Help!
+   A_Hard_Days_Night
+   Rubber_Soul
+   Magical_Mystery_Tour
+   White_Album
+   Sgt_Peppers_Lonely_Hearts_Club_Band
+   Love
+   Beatles_For_Sale
+   Yellow_Submarine).each do |name|
+  album = name.to_s
+  Record.create!(artist: 'The Beatles',
+                title: album,
+                press_date: 1960,
+                price: 15.0)
+end
 #
 # %w(antony jeff matt jason alex chelsea dave harry).each do |name|
 #   email = "#{name} is my friend"
